167577dea1a2bbebe8fddb91f51ace0428d19f77
antonio63963 <shotvideo25@gmail.com>
2024-08-13 14:46:49 +0300

    server onConnection

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import http from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 
-// import onConnection from "./socket_io/onConnection.js";
 import { getFilePath } from "./utils/file.js";
 import onError from "./utils/onError.js";
 import { uploadFile } from "./utils/upload.js";
+import onConnection from "./socket_io/onConnection.js";
 
 const app = express();
 app.use(express.json());
@@ -19,29 +19,28 @@ app.use(
   })
 );
 
-app.use('/upload', uploadFile.single('file'), (req, res) => {
-  if(!req.file) return res.sendStatus(400);
+app.use("/upload", uploadFile.single("file"), (req, res) => {
+  if (!req.file) return res.sendStatus(400);
   const relativeFilePath = req.file.path
-  .replace(/\\/g, '/')
-  .split('server/files')[1];
+    .replace(/\\/g, "/")
+    .split("server/files")[1];
 
   res.status(200).json(relativeFilePath);
 });
 
-app.use('/files', (req, res) => {
+app.use("/files", (req, res) => {
   const filePath = getFilePath(req.url);
   res.status(200).sendFile(filePath);
-})
+});
 
 app.use(onError);
 
 try {
-
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  console.log('DB was connected...')
+    useUnifiedTopology: true,
+  });
+  console.log("DB was connected...");
 } catch (error) {
   onError(error);
 }
@@ -49,16 +48,18 @@ try {
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  // onConnection(io, socket)
+  cors: process.env.ALLOWED_ORIGIN,
+  serveClient: false
 });
+
+io.on('connection', (socket) => onConnection(io, socket));
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, (err) => {
-  if(err) {
-    console.error('Server launch ERROR: ', err);
+  if (err) {
+    console.error("Server launch ERROR: ", err);
   } else {
     console.log(`Server has launched on port: ${PORT}`);
   }
-
-})
-
+});
