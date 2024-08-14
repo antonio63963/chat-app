a3f52dd8ce62810bcd2784458702bca2e33bec26
antonio63963 <shotvideo25@gmail.com>
2024-08-14 16:36:33 +0300

    message handler server

--- server/src/socket_io/handlers/message.handlers.js
+++ server/src/socket_io/handlers/message.handlers.js
@@ -1,19 +1,19 @@
-import Message from '../../models/message.model.js';
-import {removeFile} from '../../utils/file.js';
-import onError from '../../utils/onError.js';
+import Message from "../../models/message.model.js";
+import { removeFile } from "../../utils/file.js";
+import onError from "../../utils/onError.js";
 
 const messages = {};
 
 function messageHandlers(io, socket) {
-  const {roomId} = socket;
+  const { roomId } = socket;
 
   function updateMessageList() {
-    io.to(roomId).emit('message_list:update', messages[roomId]);
-  };
+    io.to(roomId).emit("message_list:update", messages[roomId]);
+  }
 
-  socket.on('message:get', async () => {
+  socket.on("message:get", async () => {
     try {
-      const _messages = await Message.find({roomId});
+      const _messages = await Message.find({ roomId });
       messages[roomId] = _messages;
       updateMessageList();
     } catch (error) {
@@ -21,25 +21,28 @@ function messageHandlers(io, socket) {
     }
   });
 
-  socket.on('message:add', (message) => {
+  socket.on("message:add", (message) => {
     Message.create(message).catch(onError);
-//переделать
+    //переделать
     message.createdAt = Date.now();
     messages[roomId].push(message);
     updateMessageList();
   });
 
-  socket.on('message:remove', (message) => {
-    const {messageId, messageType, textOrPathToFile} = message;
+  socket.on("message:remove", (message) => {
+    const { messageId, messageType, textOrPathToFile } = message;
 
-    Message.deleteOne(messageId).then(() => {
-      if(messageType !== 'text') {
-        removeFile(textOrPathToFile)
-      };
-    }).catch(onError);
+    Message.deleteOne(messageId)
+      .then(() => {
+        if (messageType !== "text") {
+          removeFile(textOrPathToFile);
+        }
+      })
+      .catch(onError);
 
-    messages[roomId] = messages[roomId].filter(m => m.id !== messageId);
+    messages[roomId] = messages[roomId].filter((m) => m.id !== messageId);
     updateMessageList();
-  })
-
+  });
 }
+
+export default messageHandlers;
