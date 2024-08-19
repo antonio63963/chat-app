e1c1cd8f80f2e353b228d266df186518105df8b4
antonio63963 <shotvideo25@gmail.com>
2024-08-19 11:25:24 +0300

    added recording util

---
+++ client/src/utils/recording.js
@@ -0,0 +1,77 @@
+let mediaRecorder = null;
+let mediaChunks = [];
+let mediaType = null;
+
+const audioConstraints = {
+  audio: {
+    echoCancellation: true,
+    autoGainControl: true,
+    noSupperssion: true,
+    channelCount: 2,
+  },
+};
+
+const videoConstraints = {
+  ...audioConstraints,
+  video: {
+    width: 1920,
+    height: 1080,
+    frameRate: 60,
+  },
+};
+
+const isRecordingStarted = () => !!mediaRecorder;
+
+const pauseRecording = () => {
+  mediaRecorder.pause();
+};
+
+//continue recording
+const resumeRecording = () => {
+  mediaRecorder.resume();
+};
+
+const startRecording = async (constraints) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+     mediaType = constraints.video ? "video" : "audio";
+
+    mediaRecorder = new MediaRecorder(stream, { mimeType: `${mediaType}/webm` });
+    mediaRecorder.ondataavailable = ({ data }) => {
+      mediaChunks.push(data);
+    };
+
+    mediaRecorder.start(500);
+    return stream;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const stopRecording = () => {
+  mediaRecorder.stop();
+  mediaRecorder.stream.getTracks().forEach((streamTrack) => {
+    streamTrack.stop();
+  });
+
+  const file = new File(mediaChunks, 'my_record.webm', {
+    type: `${mediaType}/webm`
+  });
+
+  //avoid memory leaks
+  mediaRecorder.ondataavailable = null;
+  mediaRecorder = null;
+  mediaChunks = [];
+
+  return file;
+};
+
+export {
+  audioConstraints,
+  videoConstraints,
+  isRecordingStarted,
+  pauseRecording,
+  resumeRecording,
+  startRecording,
+  stopRecording
+};
